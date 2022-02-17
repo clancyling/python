0bae9531c79df5e100c51120a7440e9b811bd4ec
clancyling <99574580+clancyling@users.noreply.github.com>
2022-02-17 22:59:16 +1000

    Add files via upload

--- 9.1 1.py
+++ 9.1 1.py
@@ -6,24 +6,27 @@ fh = open('mbox-short.txt')
 counts = dict()
 for line in fh:
     line = line.rstrip()
-    words = line.split()
+    s = line.split(':')
+    wordss = s[0]
+    words = wordss.split()
+    
+    
     if not line.startswith('From ') : continue
-    print(words)
+
     
     for word in words:
           if word == words[5]:
               counts[word] = counts.get(word,0) + 1
-print(counts)
+
+
+
 tmp = list()
 for k,v in counts.items() :
     print(k,v)
     newt = (v,k)
     tmp.append(newt)
-print('Flipped',tmp)
+
 tmp = sorted(tmp, reverse=True)
 
-print('Sorted',tmp[:0])
-for v,k in tmp[:0] :
+for v,k in tmp[:12] :
     print(k,v)
-
-
